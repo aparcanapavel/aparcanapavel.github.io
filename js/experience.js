0a529cfe66dc0c2bff32daa153c74d28f6972fec
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-14 18:44:56 -0400

    Experience Page
    finalized the second page. made i simple because the projects page will have many effects.

--- js/experience.js
+++ js/experience.js
@@ -1,3 +1,4 @@
+// first slideshow
 var myIndex = 0;
 carousel();
 
@@ -12,3 +13,30 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 2500); // Change image every 2.5 seconds
 }
+
+// second slideshpw
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slidePic");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// smooth scrolling
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
